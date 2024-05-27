4c4eae4c2d783a0e38d29d92b6a50df3dfc0a2ed
Arif Hidayat <44585278+rizentium@users.noreply.github.com>
2024-05-27 22:48:10 +0700

    feat(home): fix links in home page (#3)
    * build: replace astro to nuxtjs
    
    * feat(home): fix links for profile

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,41 @@
+<script setup>
+const profiles = [
+    {
+        url: 'https://www.linkedin.com/in/arifhidayat95',
+        logo: '/images/linkedin.webp',
+        alt: 'Linkedin Profile',
+    },
+    {
+        url: 'https://github.com/rizentium',
+        logo: '/images/github.webp',
+        alt: 'Github Profile',
+    },
+    {
+        url: 'https://stackoverflow.com/users/10576760/rize',
+        logo: '/images/stackoverflow.webp',
+        alt: 'StackOverFlow Profile',
+    },
+    {
+        url: 'https://medium.com/@af.hidayat',
+        logo: '/images/medium.webp',
+        alt: 'Medium Profile',
+    },
+];
+</script>
+
 <template>
     <div class="landing_page bg-gray-100 p-12">
         <div class="landing_page-body">
-            <NuxtImg class="rounded-full" format="webp" fit="cover" src="/images/profile_picture.webp" width="128" height="128" alt="Profile Picture" />
-            <br />
+            <NuxtImg class="rounded-full my-4" format="webp" fit="cover" src="/images/profile_picture.webp" width="128"
+                height="128" alt="Profile Picture" />
             <p class="text-2xl font-semibold">Arif Hidayat</p>
-            <br />
-            <div class="landing_page-body-socmed-section">
-                <a href="https://www.linkedin.com/in/arifhidayat95/" target="_blank">
-                    <NuxtImg class="rounded-full" format="webp" fit="cover" src="/images/linkedin.webp" width="32" height="32" alt="Linkedin Profile" />
-                </a>
-                <a href="https://www.linkedin.com/in/arifhidayat95/" target="_blank">
-                    <NuxtImg class="rounded-full" format="webp" fit="cover" src="/images/github.webp" width="32" height="32" alt="Github Profile" />
-                </a>
-                <a href="https://www.linkedin.com/in/arifhidayat95/" target="_blank">
-                    <NuxtImg class="rounded-full" format="webp" fit="cover" src="/images/stackoverflow.webp" width="32" height="32" alt="StackOverFlow Profile" />
-                </a>
-                <a href="https://www.linkedin.com/in/arifhidayat95/" target="_blank">
-                    <NuxtImg class="rounded-full" format="webp" fit="cover" src="/images/medium.webp" width="32" height="32" alt="Medium Profile" />
+            <div class="landing_page-body-socmed-section my-4">
+                <a v-for="profile in profiles" :href="profile.url" target="_blank">
+                    <NuxtImg class="rounded-full" format="webp" fit="cover" :src="profile.logo" width="32" height="32"
+                        :alt="profile.alt" />
                 </a>
             </div>
-            <br />
-            <p class="border-2 rounded-lg p-4 bg-white">
+            <p class="border-2 rounded-lg p-4 bg-white text-justify">
                 With over five years of experience in full-stack development, I have a proven track record of delivering
                 high-quality software solutions from conception to production. My expertise lies in Frontend Software
                 Engineering and adhere to the principles of clean code architecture.
